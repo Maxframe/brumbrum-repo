f043cb726eacfc02fe52e5826b732e7d96650780
siangi <siangi@gmx.ch>
2021-11-19 10:14:24 +0100

    Camera is a bit glitchy with the terrain but works mostly

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -6,26 +6,33 @@
 
 <script>
 import mapboxgl from 'mapbox-gl';
-import getCoordinatesFromGpxFile from "@/modules/gpx-utilities.js"
+import getCoordinatesFromGpxFile from "@/modules/gpx-utilities.js";
+import * as turf from '@turf/turf';
+
+const CAMERA_ALTITUDE = 1400;
+const CAMERA_DISTANCE_BACK = 500;
+let routeCoords;
+let distance = 0;
+let globalMap;
 
 export default {
     name: 'Map',
 
     mounted: async function () {
-        mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtYnVlbGhlcnpvZyIsImEiOiJja3Z0aWEwaXgwczFkMnZtOTBrcDJhM2dtIn0.wLbpWYbbUQFwlMrIlQ85BQ';
+        mapboxgl.accessToken = 'pk.eyJ1Ijoic2lhbmdpIiwiYSI6ImNrdnRqbGM3ejBzazQyb2x5ZHMwbm5uc2oifQ.VGFNc1MhMDi_q9Uh12oCYg';
 
         let map = new mapboxgl.Map({
             container: this.$refs.container,
-            style: 'mapbox://styles/sambuelherzog/ckvtiznw8229j15o76fbjedx1',
-            bearing: 350,
+            style: 'mapbox://styles/siangi/ckw65hmof0on415ppqndfa1i2',
+            bearing: 270,
             center: [8.182855, 46.878608],
-            zoom: 11
+            zoom: 17,
+            pitch: 85
         });
 
         let coordinates;
         map.on("load", async function () {
             coordinates = await getCoordinatesFromGpxFile("../assets/incompleteTrack.gpx");
-            console.log(coordinates);
             map.addSource("route", {
                 type: "geojson",
                 data: {
@@ -36,7 +43,7 @@ export default {
                     }
                 }
             });
-
+            console.log("loaded")
             map.addLayer({
                 id: "route",
                 type: "line",
@@ -50,15 +57,59 @@ export default {
                     "line-width": 8
                 }
             });
+            routeCoords = turf.cleanCoords(turf.lineString(coordinates));
+            globalMap = map;
+            moveAlongLoop();
+
+        
         });
+    
+    
     }
+}
+
+function setCameraPosition(map, routeLineString, distanceTravelled){
+    const camera = map.getFreeCameraOptions();
+    camera.position = getCameraPos(routeLineString, distanceTravelled);
+    camera.lookAtPoint(getLookAt(routeLineString, distanceTravelled));
+    map.setFreeCameraOptions(camera);
+}
+
+function moveAlongLoop(){
+    setCameraPosition(globalMap, routeCoords, distance);
+    distance += 5;
+
+    requestAnimationFrame(moveAlongLoop);
+}
+
+
+function getLookAt(routeLineString, distanceTravelled){
+    let cameraCoord = turf.along(routeLineString, distanceTravelled, {units: 'meters'}).geometry.coordinates;
+    return { lng: cameraCoord[0], lat: cameraCoord[1]};
+}
+
+function getCameraPos(routeLineString, distanceTravelled){
+    let distanceAlong;
+    if (distanceTravelled <= CAMERA_DISTANCE_BACK){
+        distanceAlong = 1;
+    } else {
+        distanceAlong = distanceTravelled - CAMERA_DISTANCE_BACK;
+    }
+    console.log(distanceTravelled + " " + distanceAlong)
+    const cameraCoord = turf.along(routeLineString, distanceAlong, {units: 'meters'}).geometry.coordinates;
+    return mapboxgl.MercatorCoordinate.fromLngLat(
+        {
+            lng: cameraCoord[0],
+            lat: cameraCoord[1]
+        },
+        CAMERA_ALTITUDE);
 }
 </script>
 
 <style src="mapbox-gl/dist/mapbox-gl.css"></style>
 <style>
    #map {
-    height: 50vh;
+    height: 95vh;
     width: 100vw;
   } 
 </style>
