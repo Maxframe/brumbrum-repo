30c9badc2c51562c9561ca4dcc8fffacce9a1589
DanielSchwager <danieleric.schwager@stud.hslu.ch>
2021-11-25 14:10:02 +0100

    Content Pages

--- src/components/ContentPage.vue
+++ src/components/ContentPage.vue
@@ -2,56 +2,50 @@
   <div class="bg-container">
     <div class="content-container">
       <div id="nav">
+        <div class="logo">
+          <router-link to="/">
+            <img src="../assets/media/logo.png" alt="Logo" />
+            <div class="page-title">
+              <span class="title-1">BRUM BRUM<br /></span>
+              <span class="title-2">TOURS<br /></span>
+              <span class="title-3">Glaubenberg</span>
+            </div>
+          </router-link>
+        </div>
+
         <ul>
-          <li><router-link to="/">Logo</router-link></li>
-          <li><router-link to="/media">Media</router-link></li>
           <li><router-link to="/about">About</router-link></li>
         </ul>
       </div>
       <div class="media-container">
-        <ul id="array-rendering">
-          <li v-for="c in contents" :key="c.titel">
-            <Content
-              :number="c.fields.nummer"
-              :title="c.fields.titel"
-              :location="c.fields.location"
-              :images="c.fields.bilder"
-            />
-            <!-- :pictures="c.fields.pictures.fields.file" -->
+        <div class="contentPage">
+          <!-- <video src="videos[0].fields.file.url" /> -->
+          <li v-for="i in images" :key="i.fields.file.url">
+            <img :src="i.fields.file.url" />
+            <div class="text-box-wrapper-wrapper">
+              <div class="text-box-wrapper">
+                <div class="text-box-container">
+                  <h1 class="text-box-title">{{ title }}</h1>
+                  <p class="text-box">{{ i.fields.description }}</p>
+                </div>
+              </div>
+            </div>
           </li>
-        </ul>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Content from "@/components/Content.vue";
-import { createClient } from "contentful";
-
 export default {
-  name: "ContentPage",
-  components: {
-    Content,
-  },
-  data: function () {
-    return {
-      contents: [],
-    };
-  },
-  created: function () {
-    let client = createClient({
-      space: "jfbiriazkehh",
-      accessToken: "PJ2rc9wfcHt-OqFmTWgRF5usmXx7_8u3qAJ2cbWDdbI",
-    });
-    client
-      .getEntries({
-        content_type: "brumbrumRoute",
-      })
-      .then((entries) => {
-        console.log(entries);
-        this.contents = entries.items;
-      });
+  name: "contentPage",
+  props: {
+    number: String,
+    title: String,
+    text: String,
+    location: String,
+    images: Array,
   },
 };
 </script>
@@ -63,37 +57,37 @@ body {
   padding: 0;
 }
 
-#nav ul {
-  display: flex;
-  flex-direction: row;
-  list-style-type: none;
-}
-
-#nav ul li {
-  padding: 0.3em 2em;
-}
-
 .bg-container {
   transform-origin: top right;
   transform: rotate(15deg) translate(0, 0);
   position: fixed;
-  width: 55em;
-  height: 130vh;
+
+  /*--- bg-container Breite (zweiten Wert auskommentieren um Stop zu aktivieren) ---*/
+
+  /* Breite für Fahrt*/
+  width: 50vw;
+  /* Breite für Stop*/
+  width: 70vw;
+
+  height: 200vh;
   background: #2d2d2d;
   overflow: hidden;
-  z-index: 1;
+  z-index: 5;
+  transition: ease-in-out 1s;
 }
 
 .content-container {
+  position: relative;
   transform-origin: top right;
   transform: rotate(-15deg) translate(0);
   width: 100%;
   height: 100vh;
-  color: #fff;
 }
 
 #nav {
-  padding: 30px;
+  padding: 2rem;
+  z-index: 10;
+  position: relative;
 }
 
 #nav a {
@@ -102,17 +96,152 @@ body {
   text-decoration: none;
 }
 
-#nav a.router-link-exact-active {
-  color: #f79e1a;
+#nav .logo a {
+  position: absolute;
+  display: flex;
+}
+
+#nav .logo img {
+  width: auto;
+  height: 10rem;
+}
+
+.page-title {
+  display: inline;
+  padding: 0.3rem 0 0 1rem;
+}
+
+.title-1 {
+  font-family: teko, sans-serif;
+  font-size: 2.4rem;
+  font-weight: 300;
+  letter-spacing: 0.15rem;
+}
+
+.title-2 {
+  font-family: teko, sans-serif;
+  font-size: 3.55rem;
+  font-weight: 500;
+  letter-spacing: 0.12rem;
+  line-height: 2.5rem;
+}
+
+.title-3 {
+  font-family: flood-std, sans-serif;
+  font-size: 1.7rem;
+  font-weight: 400;
+  line-height: 2rem;
+}
+
+#nav ul {
+  display: flex;
+  flex-direction: row-reverse;
+  list-style-type: none;
+  margin: 0;
+}
+
+#nav ul li {
+  padding: 0.3rem 2rem;
+}
+
+.media-container {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 100vh;
+}
+
+img {
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+}
+
+video {
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+}
+.text-box-wrapper-wrapper {
+  position: absolute;
+  bottom: 4rem;
+  left: 2rem;
+  right: 15rem;
+  height: 8rem;
+  overflow: hidden;
+  z-index: 10;
+}
+.text-box-wrapper {
+  transform-origin: top right;
+  transform: rotate(15deg) translate(0, 0);
+  position: absolute;
+  width: 100%;
+  height: 500%;
+  background: #0000007c;
+}
+
+.content-container {
+  transform-origin: top right;
+  transform: rotate(-15deg) translate(0);
+  width: 100%;
+  height: 100vh;
+}
+
+.text-box-container {
+  position: absolute;
+  transform-origin: top right;
+  transform: rotate(-15deg) translateY(0.8rem);
+  width: 90%;
+  height: 8rem;
+}
+
+h1.text-box-title {
+  margin: 1.2rem 1rem 0 3rem;
+  color: #f47525;
+  vertical-align: middle;
+}
+
+p.text-box-text {
+  margin: -0.3rem 1rem 1.2rem 3rem;
+  color: white;
+  vertical-align: middle;
+}
+
+.text-box::before {
+  content: " ";
+  position: absolute;
+  width: 0.2rem;
+  height: 6rem;
+  background-color: white;
+  left: 2rem;
+}
+
+.media-overlay {
+  position: absolute;
+  height: 36%;
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+  background-image: linear-gradient(to bottom, #1e1e1e 0%, #00000000 100%);
+  z-index: 6;
 }
 
 .Map {
-  position: fixed;
   z-index: 0;
 }
 
 #map {
   height: 100vh;
-  width: 100vw;
+  position: fixed;
+  right: 0;
+
+  /*--- Map Breite (zweiten Wert auskommentieren um Stop zu aktivieren) ---*/
+
+  /* Breite für Fahrt*/
+  width: calc(60vw + 30rem);
+  /* Breite für Stop*/
+  width: calc(30vw + 20rem);
+
+  transition: ease-in-out 1s;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,55 @@
 <template>
   <div class="home">
-    <Testlayout></Testlayout>
-    <!-- <ContentPage></ContentPage> -->
     <Map></Map>
+
+    <ul id="array-rendering">
+      <li v-for="c in contents" :key="c.fields.nummer">
+        <ContentPage
+          class="contentPage"
+          id="c.fields.number"
+          :number="c.fields.nummer"
+          :title="c.fields.titel"
+          :location="c.fields.location"
+          :images="c.fields.bilder"
+          :videos="c.fields.videos1"
+          :goProVideos="c.fields.goProVideos"
+        />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Map from "@/components/Map.vue";
-import Testlayout from "@/components/Testlayout.vue";
-//import ContentPage from "@/components/ContentPage.vue";
+// import Testlayout from "@/components/Testlayout.vue";
+import ContentPage from "@/components/ContentPage.vue";
+import { createClient } from "contentful";
 
 export default {
   name: "Home",
   components: {
     Map,
-    Testlayout,
+    ContentPage,
+  },
+  data: function () {
+    return {
+      contents: [],
+    };
+  },
+  created: function () {
+    let client = createClient({
+      space: "jfbiriazkehh",
+      accessToken: "PJ2rc9wfcHt-OqFmTWgRF5usmXx7_8u3qAJ2cbWDdbI",
+    });
+    client
+      .getEntries({
+        content_type: "brumbrumRoute",
+      })
+      .then((entries) => {
+        console.log(entries);
+        this.contents = entries.items;
+      });
   },
 };
 </script>
@@ -25,7 +58,9 @@ export default {
 .home {
   position: relative;
   width: 100%;
-  height:30000vh;
+  height: 30000vh;
 }
-
+.contentPage {
+  /* display: none; */
+}
 </style>
